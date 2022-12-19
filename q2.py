7eeea6762ecf8700e317b9816d582cd078d84ce9
Tasnuba Khanam Nirjana <104271651+Tasnuba106@users.noreply.github.com>
2022-12-19 15:02:48 +0600

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,11 @@
+'n= inp("Enter three nuabers:")'
+a=10
+b=5
+c=9
+if(a>b and a>c):
+    print("largest nuaber is a")
+
+elif(b>a and b>c):
+    print("The largest nuaber is b")
+else:
+    print("c is largest nuaber.")
